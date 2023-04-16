c26cb3ee1ef27e86606b23afd82779bf06963eac
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-04-16 08:23:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -141,7 +141,7 @@ Elsa
 
 
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
-▫️<a href=https://t.me/+3unDxE53RTRkZWZl> ᴄʜᴀɴɴᴇʟ </a>
+▫️<a href=https://t.me/Toogle_Search> ᴄʜᴀɴɴᴇʟ </a>
 ╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     RESTART_TXT = """
