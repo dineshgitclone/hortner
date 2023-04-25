97ab31a7c1e3983f3d06c869e32cd3f3985c4bb9
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2023-04-25 20:06:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,8 +84,8 @@ MONGO_URL = os.environ.get('MONGO_URL', "")
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
 #url links
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'rrlinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '4a9cdafdb60022dd2049c37842cb43c81e12eb34')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '9f6492a8493162f0766e14aac61515ed3cfea79a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 
 #Auto approve 
