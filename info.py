687fe24c072edf89ca71e3961498d35460fe9616
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2023-07-23 10:43:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 #url links
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '9f6492a8493162f0766e14aac61515ed3cfea79a')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 
 #Auto approve 
 #In private group or channel must enable request admin approval 
