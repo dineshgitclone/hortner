65c4ceaf7e4062d962579e64c943466909fe4b51
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2023-08-21 19:14:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
-AUTH_GROUPS = [-1001942909517, -1001729589926, -1001538932011, -1001928092042]
+AUTH_GROUPS = [-1001939338651, -1001841027304]
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID')
 # This is required for the plugins involving the file system.
